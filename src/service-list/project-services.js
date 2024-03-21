c4fa88af85dbd1c185b510d30a81291e23d77e42
vinhnlhe151197 <vinhnlhe151197@fpt.edu.vn>
2024-03-22 01:13:00 +0700

    up-animation

--- src/service-list/project-services.js
+++ src/service-list/project-services.js
@@ -1,9 +1,12 @@
-import React from "react"
-import { Image } from "antd"
-import ArrowCircleLeftOutlinedIcon from '@mui/icons-material/ArrowCircleLeftOutlined'
-import ArrowCircleRightOutlinedIcon from '@mui/icons-material/ArrowCircleRightOutlined'
-import ProjectListBannerImage from "../images/list-project-screen-banner.png"
-import './project-services.css'
+import ArrowCircleLeftOutlinedIcon from '@mui/icons-material/ArrowCircleLeftOutlined';
+import ArrowCircleRightOutlinedIcon from '@mui/icons-material/ArrowCircleRightOutlined';
+import { Image } from "antd";
+import { motion } from 'framer-motion';
+import React, { useEffect } from "react";
+import FooterComponent from '../footer';
+import HeaderComponent from '../header';
+import ProjectListBannerImage from "../images/list-project-screen-banner.png";
+import './project-services.css';
 
 const ProjectServices = () => {
    const iconStyle = {
@@ -11,102 +14,111 @@ const ProjectServices = () => {
       color: 'black',
       marginTop: 50,
       cursor: 'pointer',
-   }
-   const arrButton = document.querySelectorAll(".project-type-scroll-bar i")
-   arrButton.forEach(btn => {
-      btn.addEventListener("click", () => {
-         console.log(btn.id)
-      })
-   })
+   };
+
+   useEffect(() => {
+      const arrButton = document.querySelectorAll(".project-type-scroll-bar i");
+      arrButton.forEach(btn => {
+         btn.addEventListener("click", () => {
+            console.log(btn.id);
+         });
+      });
+   }, []);
+
    return (
-      <div className="project-service-screen">
-         <div className='list-project-screen image-banner'>
-            <Image
-               style={{ width: "100vw", height: "auto" }}
-               src={ProjectListBannerImage}
-               className='bg-white'
-               preview={false}
-            />
-         </div>
-         <div className="project-type-scroll-bar">
-            <i id="left">
-               <ArrowCircleLeftOutlinedIcon style={iconStyle} />
-            </i>
-            <ul className="carousel">
-               <li className="circle">
-                  <div className="img">
-                     <img src={ProjectListBannerImage} alt="img" />
-                  </div>
-                  <div className="bg-text">NỘI THẤT NHÀ PHỐ</div>
-               </li>
-               <li className="circle">
-                  <div className="img">
-                     <img src={ProjectListBannerImage} alt="img" />
-                  </div>
-                  <div className="bg-text">NỘI THẤT NHÀ PHỐ</div>
-               </li>
-               <li className="circle">
-                  <div className="img">
-                     <img src={ProjectListBannerImage} alt="img" />
-                  </div>
-                  <div className="bg-text">NỘI THẤT NHÀ PHỐ</div>
-               </li>
-               <li className="circle">
-                  <div className="img">
-                     <img src={ProjectListBannerImage} alt="img" />
-                  </div>
-                  <div className="bg-text">NỘI THẤT NHÀ PHỐ</div>
-               </li>
-               <li className="circle">
-                  <div className="img">
-                     <img src={ProjectListBannerImage} alt="img" />
-                  </div>
-                  <div className="bg-text">NỘI THẤT NHÀ PHỐ</div>
-               </li>
-               <li className="circle">
-                  <div className="img">
-                     <img src={ProjectListBannerImage} alt="img" />
-                  </div>
-                  <div className="bg-text">NỘI THẤT NHÀ PHỐ</div>
-               </li>
-            </ul>
-            <i id="right">
-               <ArrowCircleRightOutlinedIcon style={iconStyle} />
-            </i>
-         </div>
-         <div className="title">
-            <div className="screen-name">GÓI DỊCH VỤ</div>
-            <div className="slogan">
-               Từ những thiết kế nổi bật, thi công chính xác, chất liệu tốt nhất, ... VU GIA hứa hẹn mang đến những trải nghiệm đáng có cho khách hàng.
-               <br />
-               Đến và cảm nhận sự đẳng cấp và tận hưởng dịch vụ chăm sóc khách hàng tốt nhất ở VU GIA.
+      <>
+         <HeaderComponent />
+         <motion.div
+            className="project-service-screen"
+            initial={{ x: "100vw" }} // Chuyển từ bên phải màn hình
+   animate={{ x: 0 }} // Di chuyển sang vị trí 0, tức là giữ nguyên trên màn hình
+   exit={{ x: "-100vw" }} // Chuyển ra khỏi màn hình sang bên trái
+   transition={{ duration: 1, ease: [0.22, 1, 0.36, 1] }}
+         >
+            <div className='list-project-screen image-banner'>
+               <Image
+                  style={{ width: "100vw", height: "auto" }}
+                  src={ProjectListBannerImage}
+                  className='bg-white'
+                  preview={false} />
             </div>
-         </div>
-         <div className="content-container">
-            <div className="service-content">
+            <div className="project-type-scroll-bar">
+               <i id="left">
+                  <ArrowCircleLeftOutlinedIcon style={iconStyle} />
+               </i>
+               <ul className="carousel">
+                  <li className="circle">
+                     <div className="img">
+                        <img src={ProjectListBannerImage} alt="img" />
+                     </div>
+                     <div className="bg-text">NỘI THẤT NHÀ PHỐ</div>
+                  </li>
+                  <li className="circle">
+                  <div className="img">
+                     <img src={ProjectListBannerImage} alt="img" />
+                  </div>
+                  <div className="bg-text">NỘI THẤT NHÀ PHỐ</div>
+               </li>
+               <li className="circle">
+                  <div className="img">
+                     <img src={ProjectListBannerImage} alt="img" />
+                  </div>
+                  <div className="bg-text">NỘI THẤT NHÀ PHỐ</div>
+               </li>
+               <li className="circle">
+                  <div className="img">
+                     <img src={ProjectListBannerImage} alt="img" />
+                  </div>
+                  <div className="bg-text">NỘI THẤT NHÀ PHỐ</div>
+               </li>
+               <li className="circle">
+                  <div className="img">
+                     <img src={ProjectListBannerImage} alt="img" />
+                  </div>
+                  <div className="bg-text">NỘI THẤT NHÀ PHỐ</div>
+               </li>
+               <li className="circle">
+                  <div className="img">
+                     <img src={ProjectListBannerImage} alt="img" />
+                  </div>
+                  <div className="bg-text">NỘI THẤT NHÀ PHỐ</div>
+               </li>
+               </ul>
+               <i id="right">
+                  <ArrowCircleRightOutlinedIcon style={iconStyle} />
+               </i>
+            </div>
+            <div className="title">
+               <div className="screen-name">GÓI DỊCH VỤ</div>
+               <div className="slogan">
+                  Từ những thiết kế nổi bật, thi công chính xác, chất liệu tốt nhất, ... VU GIA hứa hẹn mang đến những trải nghiệm đáng có cho khách hàng.
+                  <br />
+                  Đến và cảm nhận sự đẳng cấp và tận hưởng dịch vụ chăm sóc khách hàng tốt nhất ở VU GIA.
+               </div>
+            </div>
+            <div className="content-container">
+               <div className="service-content">
+                  <div className="image">
+                     <Image
+                        style={{ width: "100vw", height: "auto" }}
+                        src={ProjectListBannerImage}
+                        className='bg-white'
+                        preview={false} />
+                  </div>
+                  <div className="content">
+                     <div className="text">Thiết kế và thi công trọn gói căn hộ ...</div>
+                     <button className="button" type="button">
+                        ĐỌC THÊM
+                     </button>
+                  </div>
+               </div>
+               <div className="service-content" style={{ flexDirection: 'row-reverse' }}>
                <div className="image">
                   <Image
                      style={{ width: "100vw", height: "auto" }}
                      src={ProjectListBannerImage}
                      className='bg-white'
-                     preview={false}
-                  />
-               </div>
-               <div className="content">
-                  <div className="text">Thiết kế và thi công trọn gói căn hộ ...</div>
-                  <button className="button" type="button">
-                     ĐỌC THÊM
-                  </button>
-               </div>
-            </div>
-            <div className="service-content" style={{ flexDirection: 'row-reverse' }}>
-               <div className="image">
-                  <Image
-                     style={{ width: "100vw", height: "auto" }}
-                     src={ProjectListBannerImage}
-                     className='bg-white'
-                     preview={false}
-                  />
+                     preview={false} />
                </div>
                <div className="content">
                   <div className="text">Tư vấn thiết kế sang trọng, đẹp mắt và nhanh chóng ...</div>
@@ -121,8 +133,7 @@ const ProjectServices = () => {
                      style={{ width: "100vw", height: "auto" }}
                      src={ProjectListBannerImage}
                      className='bg-white'
-                     preview={false}
-                  />
+                     preview={false} />
                </div>
                <div className="content">
                   <div className="text">Dịch vụ đăng kí báo giá xây dựng dựa trên các thiết kế tiêu chuẩn ...</div>
@@ -131,9 +142,11 @@ const ProjectServices = () => {
                   </button>
                </div>
             </div>
-         </div>
-      </div>
+            </div>
+         </motion.div>
+         <FooterComponent />
+      </>
    )
 }
 
-export default ProjectServices; 
+export default ProjectServices;
