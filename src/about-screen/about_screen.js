75430c773dd40aa5a0c1b1afdf7a7114d28aa455
daoduc220901 <90316727+concuacon1@users.noreply.github.com>
2024-04-11 19:00:58 +0700

    fix about-screen

--- src/about-screen/about_screen.js
+++ src/about-screen/about_screen.js
@@ -1,110 +1,139 @@
-import { Image } from 'antd';
-import { motion } from 'framer-motion';
-import React from 'react';
-import Video from '../../src/videos/noithatvugia.mp4';
-import { fadeIn } from '../Animation/variants';
-import FooterComponent from '../footer';
-import HeaderComponent from "../header/index";
-import AnhQuyen from '../images/image 30.png';
-import './about_screen.css';
+import React from "react"
+import { Image } from "antd"
+import ArrowCircleLeftOutlinedIcon from '@mui/icons-material/ArrowCircleLeftOutlined'
+import ArrowCircleRightOutlinedIcon from '@mui/icons-material/ArrowCircleRightOutlined'
+import ProjectListBannerImage from "../images/list-project-screen-banner.png"
+import './project-services.css'
 
-const AboutScreen = () => {
-   const ImageContentStyle = {
-      width: "470px",
-      height: "470px"
+const ProjectServices = () => {
+   const iconStyle = {
+      fontSize: 50,
+      color: 'black',
+      marginTop: 50,
+      cursor: 'pointer',
    }
+   const arrButton = document.querySelectorAll(".project-type-scroll-bar i")
+   arrButton.forEach(btn => {
+      btn.addEventListener("click", () => {
+         console.log(btn.id)
+      })
+   })
    return (
-      <div>
-         <HeaderComponent />
-         <motion.div className='about-screen'
-            initial={{ scaleY: 0 }}
-            animate={{ scaleY: 1 }} // Change from 0 to 1 to animate from scaleY 0 to scaleY 1
-            exit={{ scaleY: 1 }} // Change from 0 to 1 to animate from scaleY 0 to scaleY 1
-            transition={{ duration: 1, ease: [0.22, 1, 0.36, 1] }}
-         >
-
-            <motion.div className='about-screen title'>
-
-            </motion.div>
-            <motion.div className='Text-content'
-
-               variants={fadeIn('down', 0.2)}
-               initial="hidden"
-               whileInView={"show"}
-               viewport={{ once: false, amount: 0.7 }}
-            >
-               Doanh Nghiệp
-            </motion.div>
-
-            <div className='about-screen container'>
-               <motion.div className='images'
-                
-               >
+      <div className="project-service-screen">
+         <div className='image-banner'>
+            <Image
+               style={{ width: "100vw", height: "auto" }}
+               src={ProjectListBannerImage}
+               className='bg-white'
+               preview={false}
+            />
+         </div>
+         <div className="project-type-scroll-bar">
+            <i id="left">
+               <ArrowCircleLeftOutlinedIcon style={iconStyle} />
+            </i>
+            <ul className="carousel">
+               <li className="circle">
+                  <div className="img">
+                     <img src={ProjectListBannerImage} alt="img" />
+                  </div>
+                  <div className="bg-text">NỘI THẤT NHÀ PHỐ</div>
+               </li>
+               <li className="circle">
+                  <div className="img">
+                     <img src={ProjectListBannerImage} alt="img" />
+                  </div>
+                  <div className="bg-text">NỘI THẤT NHÀ PHỐ</div>
+               </li>
+               <li className="circle">
+                  <div className="img">
+                     <img src={ProjectListBannerImage} alt="img" />
+                  </div>
+                  <div className="bg-text">NỘI THẤT NHÀ PHỐ</div>
+               </li>
+               <li className="circle">
+                  <div className="img">
+                     <img src={ProjectListBannerImage} alt="img" />
+                  </div>
+                  <div className="bg-text">NỘI THẤT NHÀ PHỐ</div>
+               </li>
+               <li className="circle">
+                  <div className="img">
+                     <img src={ProjectListBannerImage} alt="img" />
+                  </div>
+                  <div className="bg-text">NỘI THẤT NHÀ PHỐ</div>
+               </li>
+               <li className="circle">
+                  <div className="img">
+                     <img src={ProjectListBannerImage} alt="img" />
+                  </div>
+                  <div className="bg-text">NỘI THẤT NHÀ PHỐ</div>
+               </li>
+            </ul>
+            <i id="right">
+               <ArrowCircleRightOutlinedIcon style={iconStyle} />
+            </i>
+         </div>
+         <div className="title">
+            <div className="screen-name">GÓI DỊCH VỤ</div>
+            <div className="slogan">
+               Từ những thiết kế nổi bật, thi công chính xác, chất liệu tốt nhất, ... VU GIA hứa hẹn mang đến những trải nghiệm đáng có cho khách hàng.
+               <br />
+               Đến và cảm nhận sự đẳng cấp và tận hưởng dịch vụ chăm sóc khách hàng tốt nhất ở VU GIA.
+            </div>
+         </div>
+         <div className="content-container">
+            <div className="service-content">
+               <div className="image">
                   <Image
-                     style={ImageContentStyle}
-                     src={AnhQuyen}
+                     style={{ width: "100vw", height: "auto" }}
+                     src={ProjectListBannerImage}
                      className='bg-white'
-                     preview={true}
+                     preview={false}
                   />
-               </motion.div>
-               <motion.div className="content"
-
-
-
-               >
-                  Chúng ta đã có nhiều năm gia công cho các thương hiệu nội thất lớn trên thế giới. Các sản phẩm được sản xuất tại Việt Nam, gắn thương hiệu ngoại và quay trở lại bán cho người Việt với giá gấp nhiều lần giá trị sử dụng thực.
-                  <br /><br />Xuất phát từ lòng tự hào dân tộc, khởi nguồn từ khao khát người Việt Nam được dùng hàng hiệu Việt với tiêu chuẩn châu Âu cùng mức chi phí tối ưu. Vũ Gia ra đời với đầy đủ phẩm chất của một thương hiệu gia công chế tác Nội thất cao cấp.
-                  <br /><br />Vũ Gia Furniture nâng tầm phong cách sống sang trọng, đẳng cấp với những giá trị bền vững đi cùng mức chi phí hợp lý.
-               </motion.div>
-               <div className='data'>
-                  <motion.div className='item'
-                     variants={fadeIn('right', 0.2)}
-                     initial="hidden"
-                     whileInView={"show"}
-                     viewport={{ once: false, amount: 0.7 }}
-                  >
-                     <p style={{ fontSize: "40px", color: '#A14C17' }}>1 Tỷ</p>
-                     <p>Hoàn trả nếu không đúng nguyên vật liệu</p>
-                  </motion.div>
-                  <motion.div className='item'
-                     variants={fadeIn('up', 0.2)}
-                     initial="hidden"
-                     whileInView={"show"}
-                     viewport={{ once: false, amount: 0.7 }}
-                  >
-                     <p style={{
-                        fontSize: "40px",
-                        color: '#A14C17'
-                     }}>5 năm</p>
-                     <p> Bảo hành sản phẩm </p>
-
-                  </motion.div>
-                  <motion.div className='item'
-                     variants={fadeIn('left', 0.2)}
-                     initial="hidden"
-                     whileInView={"show"}
-                     viewport={{ once: false, amount: 0.7 }}
-                  >
-                     <p style={{
-                        fontSize: "40px",
-                        color: '#A14C17'
-                     }}>100%</p>
-                     <p> Cam kết chính xác so với hồ sơ thiết kế </p>
-                  </motion.div>
+               </div>
+               <div className="content">
+                  <div className="text">Thiết kế và thi công trọn gói căn hộ ...</div>
+                  <button className="button" type="button">
+                     ĐỌC THÊM
+                  </button>
                </div>
             </div>
-            <div className='about-screen slogan'>
-               <span style={{ textAlign: 'left' }}>NỘI THẤT VŨ GIA _</span>
-               <span style={{ textAlign: 'right' }}>_ ĐEM ƯỚC MƠ VỀ TỚI CỬA NHÀ</span>
+            <div className="service-content" style={{ flexDirection: 'row-reverse' }}>
+               <div className="image">
+                  <Image
+                     style={{ width: "100vw", height: "auto" }}
+                     src={ProjectListBannerImage}
+                     className='bg-white'
+                     preview={false}
+                  />
+               </div>
+               <div className="content">
+                  <div className="text">Tư vấn thiết kế sang trọng, đẹp mắt và nhanh chóng ...</div>
+                  <button className="button" type="button">
+                     ĐỌC THÊM
+                  </button>
+               </div>
             </div>
-            <div className='about-screen video'>
-               <video autoPlay loop muted style={{ width: '100vw' }}>
-                  <source src={Video} type='video/mp4'></source>
-               </video>
+            <div className="service-content">
+               <div className="image">
+                  <Image
+                     style={{ width: "100vw", height: "auto" }}
+                     src={ProjectListBannerImage}
+                     className='bg-white'
+                     preview={false}
+                  />
+               </div>
+               <div className="content">
+                  <div className="text">Dịch vụ đăng kí báo giá xây dựng dựa trên các thiết kế tiêu chuẩn ...</div>
+                  <button className="button" type="button">
+                     ĐỌC THÊM
+                  </button>
+               </div>
             </div>
-            <FooterComponent />
-         </motion.div>
+         </div>
       </div>
    )
-}
-export default AboutScreen;
+};
+
+export default ProjectServices;             
