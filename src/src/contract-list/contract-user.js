f83ce292589e9bff7c0d7e1c6c6935e11b7f9a86
vinhnlhe151197 <vinhnlhe151197@users.noreply.github.com>
2024-04-15 21:34:18 +0700

    up contract-user

--- src/src/contract-list/contract-user.js
+++ src/src/contract-list/contract-user.js
@@ -1,28 +1,33 @@
 import { Card, Image } from 'antd';
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import { ToastContainer } from 'react-toastify';
 import instance from "../configApi/axiosConfig";
 import FooterComponent from "../footer/index";
 import HeaderComponent from "../header/index";
-import VuGia from "../images/Vu_gia.png";
+import { formatDate } from "../helper/formatDate";
+import Contract from "../images/contract.png";
 
 const { Meta } = Card;
 
-const ScheduleCard = () => {
+const ScheduleCard = ({ contract }) => {
     return (
         <Card style={{ marginBottom: 16 }}>
             <div style={{ display: 'flex', justifyContent: 'center', background: 'rgba(102, 112, 133, 0.20)', padding: '50px', margin: '0 auto' }}>
                 <div style={{ width: '182px', height: '210px', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', alignItems: 'center' }}>
                     <Image
                         height={'100%'}
-                        src={VuGia}
+                        src={Contract}
                         preview={false}
                     />
                 </div>
                 <div style={{ marginLeft: 100, fontSize: 40, fontWeight: 500, color: 'black', fontFamily: 'Roboto' }}>
-                    <p>Hợp đồng thiết kế</p>
-                    <p>Ngày 20/12/2017</p>
-                    <button>Xem chi tiết</button>
+                    <p>{contract.nameContract}</p>
+                    <p>Kiến trúc sư: {contract.designerData[0].fullName}</p>
+                    <p>Ngày {formatDate(contract.timeSigned)}</p>
+                    <button style={{ height: '40px' }} className="custombutton-register-designer">
+                        <Link to={`/contract/${contract._id}`} target="_blank">Xem chi tiết</Link>
+                    </button>
                 </div>
             </div>
         </Card >
@@ -32,14 +37,14 @@ const ScheduleCard = () => {
 const ContractUser = () => {
 
     const [listContract, setListContract] = useState([]);
+    const [message, setMessage] = useState('');
 
     useEffect(() => {
         async function fetchData() {
             try {
                 const calendarRes = await instance.get('/list_contract_user');
-                console.log('calendarRes == ', calendarRes);
                 if (calendarRes.data.data.listContract.length === 0) {
-                    
+                    setMessage('Bạn chưa có hợp đồng nào với công ty')
                 }
                 setListContract(calendarRes.data.data.listContract)
             } catch (error) {
@@ -53,11 +58,19 @@ const ContractUser = () => {
         <div className="h-screen" style={{ minHeight: '100vh', display: 'flex', flexDirection: 'column' }}>
             <HeaderComponent />
             <ToastContainer />
-            <main style={{ flex: 1 }}>
-                <ScheduleCard />
+            <main style={{ display: 'flex', justifyContent: 'space-around' }}>
+                {
+                    listContract.length > 0 && listContract.map((contract, index) => (
+                        <ScheduleCard key={index} contract={contract} />
+                    ))
+                }
+                {
+                    message && <div style={{ marginTop: '200px', fontSize: '40px' }}>{message}</div>
+                }
             </main>
-            <div className="border-2 pt-2 pb-2"></div>
-            <FooterComponent />
+            <div style={{ position: 'absolute', bottom: 0, width: '100%' }}>
+                <FooterComponent />
+            </div>
         </div>
     );
 };
