13504a41cd4f2dae4cf85bd813ab608ba7fe98e4
vinhnlhe151197 <vinhnlhe151197@users.noreply.github.com>
2024-04-15 23:57:51 +0700

    up chatbot

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import 'react-toastify/dist/ReactToastify.css';
 import AboutScreen from './about-screen/about_screen';
 import AccountList from "./account-list/index";
 import ChangePassword from "./change-password/index";
+import ChatBot from './chat-bot';
 import ChatList from './chat-box/general-chat';
 import ContractDetail from './contract-detail';
 import ContractList from './contract-list';
@@ -15,6 +16,7 @@ import DesignerDetail from './designer-detail';
 import DesignerList from './designer-list';
 import DesignerSchedule from './designer-schedule';
 import EditPasswordOtp from './edit-password-otp';
+import { LoadingOverlay } from './helper/loadingOverlay';
 import HomePage from "./homePage/index";
 import './index.css';
 import Login from "./login/index";
@@ -35,43 +37,58 @@ import UserSchedule from './user-schedule/UserSchedule';
 import WorkingProject from './working-project';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/register" element={<Register />} />
-        <Route path="/register-customer" element={<RegisterCustomer />} />
-        <Route path="/register-designer" element={<RegisterDesigner />} />
-        <Route path="/edit-password" element={<ChangePassword />} />
-        <Route path="/edit-password-otp" element={<EditPasswordOtp />} />
-        <Route path="/home-page" element={<HomePage />} />
-        <Route path="/list-user-admin" element={<AccountList />} />
-        <Route path="/list-user-staff" element={<StaffList />} />
-        <Route path="/list-user-designer" element={<DesignerList />} />
-        <Route path="/cv/:designerId" element={<DesignerDetail />} />
-        <Route path="/list-user-customer" element={<CustomerList />} />
-        <Route path="/list-schedule" element={<ScheduleList />} />
-        <Route path="/list-contract" element={<ContractList />} />
-        <Route path="/contract-user" element={<ContractUser />} />
-        <Route path="/contract/:contract_id" element={<ContractDetail />} />
-        <Route path="/create-project" element={<CreateProject />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/about-screen" element={<AboutScreen />} />
-        <Route path="/project-list/:project_type" element={<ListProject />} />
-        <Route path="/project/:project_id" element={<ProjectValue />} />
-        <Route path="/service" element={<ProjectServices />} />
-        <Route path="/service/01" element={<Service1 />} />
-        <Route path="/service/02" element={<Service2 />} />
-        <Route path="/service/03" element={<Service3 />} />
-        <Route path="/working-profile" element={<WorkingProject />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/schedule/:designer_id" element={<DesignerSchedule />} />
-        <Route path="/create-schedule" element={<CreateSchedule />} />
-        <Route path="/user-schedule" element={<UserSchedule />} />
-        <Route path="/list-chatbox/:staff_id" element={<ChatList />} />
-        <Route path="/policiesandtermsofservice" element={<PoliciesService />} />
+  const [role, setRole] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
-      </Routes>
-    </BrowserRouter>
+  useEffect(() => {
+    setIsLoading(true);
+    const roleData = JSON.parse(localStorage.getItem('datawebfpt'))?.role || '';
+    setRole(roleData);
+    setIsLoading(false);
+  }, []);
+
+  return (
+    <>
+      <BrowserRouter>
+        {
+          isLoading && <LoadingOverlay />
+        }
+        {role !== 'ADMIN' && role !== 'STAFF' && <ChatBot />}
+        <Routes>
+          <Route path="/register" element={<Register />} />
+          <Route path="/register-customer" element={<RegisterCustomer />} />
+          <Route path="/register-designer" element={<RegisterDesigner />} />
+          <Route path="/edit-password" element={<ChangePassword />} />
+          <Route path="/edit-password-otp" element={<EditPasswordOtp />} />
+          <Route path="/home-page" element={<HomePage />} />
+          <Route path="/list-user-admin" element={<AccountList />} />
+          <Route path="/list-user-staff" element={<StaffList />} />
+          <Route path="/list-user-designer" element={<DesignerList />} />
+          <Route path="/cv/:designerId" element={<DesignerDetail />} />
+          <Route path="/list-user-customer" element={<CustomerList />} />
+          <Route path="/list-schedule" element={<ScheduleList />} />
+          <Route path="/list-contract" element={<ContractList />} />
+          <Route path="/contract-user" element={<ContractUser />} />
+          <Route path="/contract/:contract_id" element={<ContractDetail />} />
+          <Route path="/create-project" element={<CreateProject />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/about-screen" element={<AboutScreen />} />
+          <Route path="/project-list/:project_type" element={<ListProject />} />
+          <Route path="/project/:project_id" element={<ProjectValue />} />
+          <Route path="/service" element={<ProjectServices />} />
+          <Route path="/service/01" element={<Service1 />} />
+          <Route path="/service/02" element={<Service2 />} />
+          <Route path="/service/03" element={<Service3 />} />
+          <Route path="/working-profile" element={<WorkingProject />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/schedule/:designer_id" element={<DesignerSchedule />} />
+          <Route path="/create-schedule" element={<CreateSchedule />} />
+          <Route path="/user-schedule" element={<UserSchedule />} />
+          <Route path="/list-chatbox/:staff_id" element={<ChatList />} />
+          <Route path="/policiesandtermsofservice" element={<PoliciesService />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 
 }
